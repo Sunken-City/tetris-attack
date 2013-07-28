dd1754a6526a54d40ed5c5a666c9b632d7094324
unknown <acloudy@mail.smu.edu>
2013-07-28 15:08:56 -0500

    This visit to Cafe Brazil has done me good. Blocks now fall properly (At least, it appears so.)

--- Tetris Attack/Tetris Attack/Tetris Attack/TetrisAttackServer/Cursor.cs
+++ Tetris Attack/Tetris Attack/Tetris Attack/TetrisAttackServer/Cursor.cs
@@ -26,9 +26,20 @@ namespace Tetris_Attack
 			for (int i = 0; i < 9; i++)
 			{
 				var block = blockList.ElementAt(i);
-				if (block.Type != BlockTypes.Empty)
+				if (block.Type != BlockTypes.Empty && i != 0)
 				{
-
+					int blockCounter = 1;
+					var previousBlock = blockList.ElementAt(i - blockCounter);
+					while (previousBlock.Type == BlockTypes.Empty)
+					{
+						blockList.Remove(previousBlock);
+						blockList.AddLast(previousBlock);
+						if (blockCounter != i)
+						{
+							blockCounter++;
+						}
+						previousBlock = blockList.ElementAt(i - blockCounter);
+					}
 				}
 			}
 		}
